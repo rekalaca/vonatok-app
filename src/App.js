56441b5d4a95aa535c534a85bb7a6fb7b389afc6
rekalaca <rekalaca@gmail.com>
2022-02-17 19:05:48 +0100

    még hibás

--- src/App.js
+++ src/App.js
@@ -10,11 +10,24 @@ function App() {
       .then((data) => setTrains(data))
   }, [])
 
+  const handleChange = (e) => {
+    fetch(`http://localhost:4444/vonat?vonatId=${e.target.value}`), {
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      id: e.target.value,
+    }),
+
+  })
+
   return (
     <div className='container'>
       <div className='row'>
         <div className='col-12'>
-          <select className="form-select" aria-label="Default select example" defaultValue="">
+          <select className="form-select" aria-label="Default select example" defaultValue=""
+            onChange={(e) => handleChange(e)}>
             {trains.map((train) => 
               <option key={train.vId} value={train.vId}>
                 {train.vNev}
@@ -27,6 +40,7 @@ function App() {
       </div>
     </div>
   );
+}
 }
 
 export default App;
